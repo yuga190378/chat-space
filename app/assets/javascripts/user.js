73ef8f5ddb7071a3d1f763c888b62f9c4326e1dc
yuga190378 <gayokunomiko@gmail.com>
2019-09-05 17:31:58 +0900

    complete incremental search

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,58 @@
+$(function(){
+  var search_list = $("#user-search-result");
+  function appendUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>`
+    search_list.append(html);
+  }
+
+  function appendErrMsgToHTML(msg){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${msg}</p>
+                </div>`
+    search_list.append(html);
+  }
+
+  $("#user-search-field").on("keyup", function(){
+    var input = $("#user-search-field").val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendErrMsgToHTML("一致するユーザーが見つかりません");
+      }
+    })
+    .fail(function() {
+      alert('ユーザー検索に失敗しました');
+    })
+  });
+
+  $(document).on("click", ".user-search-add", function(){
+    var user_id = $(this).attr('data-user-id');
+    var user_name = $(this).attr('data-user-name');
+    var select_list = $(".chat-group-users");
+    var html = `<div class='chat-group-user'>
+                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                  <p class='chat-group-user__name'>${user_name}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`
+    select_list.append(html);
+    $(this).parent().remove();
+  });
+
+  $(document).on("click", ".user-search-remove", function(){
+    $(this).parent().remove();
+  });
+});
